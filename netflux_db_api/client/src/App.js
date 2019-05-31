a6329445292c46597cb214b19b1409648f367dc3
Jack Gay <jackpowis@gmail.com>
2019-05-31 15:40:26 -0400

    second commit: still working on front end

--- netflux_db_api/client/src/App.js
+++ netflux_db_api/client/src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      apiData: null,
+      apiDataLoaded: false,
+    };
+  }
+
+  componentDidMount = async () => {
+    const movies = await axios.get('http://localhost:4567/movies');
+    const apiData = movies.data;
+  }
+  
+
+
+  showMoviesOnPage() {
+    return this.state.apiData.map((movie) => {
+      return (
+        <div className="movie" key={movie.title}>
+          <p className="content">{movie.year}</p>
+          <span className="author">{movie.genre}</span>
+          <span className="category">{movie.director}</span>
+          <span className="category">{movie.plot}</span>
+          <img className="image" src={movie.image} />
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+          {(this.state.apiDataLoaded) ? this.showQuotesOnPage() : <p>Loading...</p>}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
